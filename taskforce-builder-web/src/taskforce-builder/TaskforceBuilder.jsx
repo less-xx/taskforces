c8b8d8ca578bd3b8671613d8beee0114225b91a7
Less Jiang <oursleepless@gmail.com>
2019-05-04 23:16:47 +0800

    add blockly

--- taskforce-builder-web/src/taskforce-builder/TaskforceBuilder.jsx
+++ taskforce-builder-web/src/taskforce-builder/TaskforceBuilder.jsx
@@ -1,32 +1,35 @@
+import React, { Component } from 'react';
 import Blockly from 'node-blockly/browser';
+import {
+    Container,
+    Row,
+    Col
+} from 'react-bootstrap';
 
-function TaskforceBuilder() {
+class TaskforceBuilder extends Component {
 
+    componentDidMount() {
+        var toolboxXml = `<xml id="toolbox" style="display: none">
+                <category name="Control">
+                    <block type="controls_if"></block>
+                    <block type="controls_whileUntil"></block>
+                </category>
+                <category name="Logic">
+                    <block type="logic_compare"></block>
+                    <block type="logic_operation"></block>
+                    <block type="logic_boolean"></block>
+                </category>
+        </xml>`;
+        var xml = Blockly.Xml.textToDom(toolboxXml);
+        console.log(xml);
+        var workspace = Blockly.inject('blocklyDiv', { toolbox: xml });
+        console.log(workspace);
+        Blockly.Xml.domToWorkspace(xml, workspace);
+    }
+    render() {
+        console.log("render()");
+        return <div id="blocklyDiv" style={{ height: "600px", width: "800px" }}></div>;
+    }
 }
 
-function getToolbox() {
-    return <xml id="toolbox" style="display: none">
-        <category name="Core">
-            <category name="Control">
-                <block type="controls_if"></block>
-                <block type="controls_whileUntil"></block>
-            </category>
-            <category name="Logic">
-                <block type="logic_compare"></block>
-                <block type="logic_operation"></block>
-                <block type="logic_boolean"></block>
-            </category>
-        </category>
-        <category name="Custom">
-            <block type="start"></block>
-            <category name="Move">
-                <block type="move_forward"></block>
-                <block type="move_backward"></block>
-            </category>
-            <category name="Turn">
-                <block type="turn_left"></block>
-                <block type="turn_right"></block>
-            </category>
-        </category>
-    </xml>;
-}
+export default TaskforceBuilder;
