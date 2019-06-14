00a4a2d1e08773061c3211ce660c85f96bd6b535
jiangl <less.jiang@emc.com>
2019-06-14 17:36:33 +0800

    Update TaskforceExecutionService.java

--- taskforce-service/src/main/java/org/teapotech/taskforce/service/TaskforceExecutionService.java
+++ taskforce-service/src/main/java/org/teapotech/taskforce/service/TaskforceExecutionService.java
@@ -7,11 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,10 +42,23 @@ public class TaskforceExecutionService {
 		Queue eventQueue = new Queue(queueName);
 		String routingKey = "workspace." + workspace.getId() + ".#";
 		Binding binding = BindingBuilder.bind(eventQueue).to(eventExchange).with(routingKey);
+		rabbitAdmin.declareBinding(binding);
+		LOG.info("Event binding: {}", binding);
 
 		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
 		container.setConnectionFactory(rabbitConnectionFactory);
 		container.setQueues(eventQueue);
-		container.setMessageConverter(new Jackson2JsonMessageConverter());
+		MessageListenerAdapter listener = new MessageListenerAdapter() {
+			@Override
+			public void onMessage(Message message) {
+				// TODO Auto-generated method stub
+				super.onMessage(message);
+			}
+		};
+		listener.setMessageConverter(new Jackson2JsonMessageConverter());
+		container.setMessageListener(listener);
+
+		// TODO run
+
 	}
 }
